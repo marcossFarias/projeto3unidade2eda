1d455d1d2dc4b5c0af65b0d325aa8926b0498cdb
marcossFarias <marcossouzacg2004@proton.me>
2024-06-16 15:12:02 -0400

    ADD readFileToList

--- src/main/java/com/uepb/utils/FileUitls.java
+++ src/main/java/com/uepb/utils/FileUitls.java
@@ -1,9 +1,13 @@
 package com.uepb.utils;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A utility class for operations with files.
@@ -35,5 +39,23 @@ public class FileUitls {
         writer.newLine();
       }
     }
+  }
+
+  /**
+   * Reads the contents of a file and returns a list of strings.
+   *
+   * @param file The file to read from.
+   * @return A list of strings containing the lines read from the file.
+   * @throws IOException If an I/O error occurs while reading the file.
+   */
+  public static List<String> readFileToList(File file) throws IOException {
+    List<String> lines = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        lines.add(line);
+      }
+    }
+    return lines;
   }
 }
